bbad2964a3ee5f8752e82f2d7a6f210a83022082
afieqhamieza <afieqhaazemi@gmail.com>
2021-05-11 02:12:37 -0400

    file structures

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on but it wont connect to github
+        your app! this seems like it is gonna work</Text>
       <StatusBar style="auto" />
     </View>
   );
